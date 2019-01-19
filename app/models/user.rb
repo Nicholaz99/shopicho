4794ebdfab50330a7d3e5bb2804c6bb95ffe4c4b
Nicholas R.P <nicholasmagbanua@gmail.com>
2019-01-19 19:18:44 +0700

    add admin role on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  # validations
+  validates_presence_of :name, :email, :password_digest, :balance
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_19_062717) do
+ActiveRecord::Schema.define(version: 2019_01_19_075429) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_01_19_062717) do
     t.decimal "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end

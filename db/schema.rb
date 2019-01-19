febd5f990bb0f9133de965f7f1950745abee6a57
Nicholas R.P <nicholasmagbanua@gmail.com>
2019-01-19 14:00:43 +0700

    add JWT authorization

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_18_185442) do
+ActiveRecord::Schema.define(version: 2019_01_19_062717) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.decimal "price"
     t.integer "inventory_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.decimal "balance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

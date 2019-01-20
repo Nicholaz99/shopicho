8032709608ef4ceabb738402694b6f4bcada1137
Nicholas R.P <nicholasmagbanua@gmail.com>
2019-01-20 17:04:26 +0700

    modify secrey key

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,12 +1,13 @@
 class JsonWebToken
   class << self
+    HMAC_KEY = ENV['SECRET_KEY_BASE'] ? ENV['SECRET_KEY_BASE'] : Rails.application.credentials.secret_key_base
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, Rails.application.credentials.secret_key_base)
+      JWT.encode(payload, HMAC_KEY)
     end
  
     def decode(token)
-      body = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+      body = JWT.decode(token, HMAC_KEY)[0]
       HashWithIndifferentAccess.new body
     rescue
       nil
